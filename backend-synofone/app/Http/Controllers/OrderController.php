b6fb443b7523328b5c16dd8784393ecf79e6216e
adridho94 <adridho1994@gmail.com>
2024-08-01 03:47:11 +0700

    order

--- backend-synofone/app/Http/Controllers/OrderController.php
+++ backend-synofone/app/Http/Controllers/OrderController.php
@@ -10,31 +10,11 @@ class OrderController extends Controller
 {
     public function index()
     {
-        $orders = Order::all();
-        return response()->json([
-            'message' => $orders->count() > 0 ? 'Data Order Sebagai Berikut' : 'Data Order Belum ada',
-            'data' => $orders->count() > 0 ? $orders : []
-        ], 200);
+       
     }
 
     public function store(Request $request)
     {
-        $rules = [
-            'cart_id' => 'required',
-            'alamat' => 'required',
-            'metode_pengiriman' => 'required',
-            'metode_pembayaran' => 'required',
-        ];
-        $validator = Validator::make($request->all(), $rules);
-        if ($validator->fails()) {
-            return response()->json([
-                'message' => $validator->errors()
-            ], 400);
-        }
-        $order = Order::create($request->all());
-        return response()->json([
-            'message' => 'Data Order Berhasil di Tambahkan',
-            'data' => $order
-        ], 201);
+        
     }
 }
